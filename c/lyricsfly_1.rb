3f3b440f0eaaee62973d5463a3b6d165c4321f96
claudiob <claudiob@gmail.com>
2009-10-01 19:48:38 +0200

    Corrected  with

--- c/lyricsfly_1.rb
+++ c/lyricsfly_1.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'rexml/document'
 require "#{File.dirname(__FILE__)}/lyricsfly_key"
 
-url = "http://lyricsfly.com/api/api.php?a=Rihanna&t=Umbrella&i=#{$api_key}"
+url = "http://lyricsfly.com/api/api.php?a=Rihanna&t=Umbrella&i=#{$lyricsfly_key}"
 result = Net::HTTP.get_response(URI.parse(url))
 
 response = REXML::Document.new(result.body).elements['//tx']
