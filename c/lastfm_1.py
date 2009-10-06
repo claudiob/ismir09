56cb21f21c87653315a4722180bde4db825c0371
claudiob <claudiob@gmail.com>
2009-10-06 23:32:21 +0200

    Cleaned and commented code, still too long for live writing

--- c/lastfm_1.py
+++ c/lastfm_1.py
@@ -5,59 +5,51 @@ import pylast
 from lastfm_key import lastfm_key
 
 def recommend_artist(username):
+    # 1. Look for already known artists
     network = pylast.get_lastfm_network(api_key = lastfm_key)
     user = network.get_user(username)
-    known_artists = get_known_artists(user, 1, int(time()))
+    known_artists = get_known_artists(user)
     
-    friends = user.get_friends()[:10]
-    print "[%s has %d friends]" % (username, len(friends))
+    # 2. Retrieve list of friends
+    friends = user.get_friends()[:15]
+    print "[Analysing %d friends of %s]" % (username, len(friends))
 
+    # 3. Look for artists listened by friends one month ago or this month
     now = int(time())
-    sec_in_month = 604800*4
-    prev_artists = get_friends_artists(friends, now-sec_in_month*2, now-sec_in_month)
-    next_artists = get_friends_artists(friends, now-sec_in_month, now)
+    sec_in_month = 60*60*24*30
     
-    prev_values = sum(prev_artists.values(), [])
-    next_values = sum(next_artists.values(), [])
-    both_artists = {}
-    for artist in prev_values:
-        both_artists[artist] = float(next_values.count(artist))/prev_values.count(artist) - 1
+    old_data = {} 
+    new_data = {}
+    for friend in friends:
+        old_data[friend] = get_known_artists(friend, now-sec_in_month*2, now-sec_in_month)
+        new_data[friend] = get_known_artists(friend, now-sec_in_month, now)
+
+    # 4. Identify artists unknown to user and check if they have become popular
+    old_artists = sum(old_data.values(), [])
+    new_artists = sum(new_data.values(), [])
+    unknown_artists = list(set(old_artists) & set(new_artists) - set(known_artists))
+    unknown_data = {}
+    for artist in unknown_artists:
+        unknown_data[artist] = float(new_artists.count(artist))/old_artists.count(artist)
     
-    hottest_artists = both_artists.keys()
-    hottest_artists.sort(reverse = True, cmp = \
-        lambda x,y: cmp(both_artists[x], both_artists[y]))
-    print "[%d friends know %d artists]" % (len(friends), len(hottest_artists))
-    
-    recommended = [artist for artist in hottest_artists if artist not in known_artists][0]
-    
-    prev_listeners = [friend for friend, artists in prev_artists.items() if recommended in artists]
-    next_listeners = list(set([friend for friend, artists in next_artists.items() if recommended in artists]) - set(prev_listeners))
-    print "%s should listen to %s since" % (username, recommended)
-    print "%s already listened to %s" % (prev_listeners, recommended)
-    print "%s started listening to %s this month" % (next_listeners, recommended)
+    # 5. Sort unknown artists based on how more popular they have become 
+    unknown_artists.sort(reverse = True, cmp = lambda x,y: cmp(unknown_data[x], unknown_data[y]))
+
+    # 6. Recommend the first most trendy artists unknown to the user
+    print "%s should listen to:" % username
+    for recommend in unknown_artists[:3]:
+        old_listeners = [friend for friend, artists in old_data.items() if recommend in artists]
+        new_listeners = list(set([friend for friend, artists in new_data.items() if recommend in artists]) - set(old_listeners))
+        print "%s, already known by %s and recently discovered by %s." % (recommend, old_listeners, new_listeners)
 
 
-def get_known_artists(user, from_time='', to_time=''):
+def get_known_artists(user, from_time=1, to_time=int(time())):
     charts = user.get_weekly_artist_charts(from_time, to_time)
     return [item.item for item in charts]
 
-
-def get_friends_artists(friends, from_time, to_time):
-    friends_artists  = {}
-    for friend in friends:
-        friends_artists[friend] = get_known_artists(friend, from_time, to_time)
-    print "[Read list of friends' monthly artists]"  
-    return friends_artists
-    
 
 if __name__ == "__main__":
     username = sys.argv[1]
     recommend_artist(username)
 
-
-# To install pylast:
-# $ wget http://pypi.python.org/packages/source/p/pylast/pylast-0.4.15.tar.gz
-# $ tar xvf pylast-0.4.15.tar.gz
-# $ cd pylast-0.4.15
-# $ sudo python setup.py install
     
